bba09363e5c52cf69a77dce437ba4c15014636eb
Muhammad Uzair <149560339+ItxUzair1@users.noreply.github.com>
2024-07-07 16:49:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,227 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+  };
+  
+
+
+let select1 = document.querySelector("#from");
+let select2 = document.querySelector("#To");
+let input=document.querySelector("input");
+
+let button= document.querySelector("button");
+let img1=document.getElementById("From-Image");
+let img2=document.getElementById("To-Image");
+
+
+
+select1.addEventListener("click",(e)=>{
+    let fc=select1.value;    
+
+    if(fc=="EUR"){
+        img1.src="https://www.usflagsupply.com/images/detailed/18/europe-flag_uh4m-w8.jpg";
+    }else{
+    img1.src=`https://flagsapi.com/${fc.slice(0,2)}/flat/64.png`;
+    }
+});
+
+
+select2.addEventListener("click",()=>{
+    let tc=select2.value;
+    if(tc=="EUR"){
+    img2.src="https://www.usflagsupply.com/images/detailed/18/europe-flag_uh4m-w8.jpg";    
+    }else{
+    img2.src=`https://flagsapi.com/${tc.slice(0,2)}/flat/64.png`;
+    }
+});
+
+  
+
+let options=document.querySelectorAll(".country-options select");
+for(let i=0;i<options.length;i++){
+for(countrycode in countryList){
+    let optiontag=`<option value=${countrycode}>${countrycode}</option>`;
+    options[i].insertAdjacentHTML("beforeend",optiontag);
+}
+}
+
+button.addEventListener("click",()=>{
+    getExChangeRate().then((result)=>{
+    let display=document.querySelector(".Exchange");
+    let from=select1.value;
+    let to=select2.value;
+    let i=input.value;
+    display.innerText=`${i} ${from} = ${Math.floor(result)} ${to}`;
+   });
+})
+
+
+
+
+async function getExChangeRate(){
+    let from=select1.value.toLowerCase();
+    let url=`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from.toLowerCase()}.json`;
+    let response= await fetch(url);
+    let data= await response.json();
+    let to=select2.value.toLowerCase();
+    let v=data[from][to];
+    let inputamount = input.value;
+    return v*inputamount;
+}
